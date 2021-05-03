556bca7f38d474d7b9ceecf625a5ba7fe3c2a4ec
Olim-Jabborov <olimjonjabborov0701@gmail.com>
2021-05-03 17:36:18 +0500

    Add post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    date = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='event_images/')
--- myawesomeblog_loyiha/settings.py
+++ myawesomeblog_loyiha/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
